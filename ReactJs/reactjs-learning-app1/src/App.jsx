cf4d6f0e866b1d0e1941e9dc141c1e2a3416476a
davidconqueror <davidpconqueror@gmail.com>
2025-02-09 00:02:03 +0530

    implement movie card and search functionality

--- ReactJs/reactjs-learning-app1/src/App.jsx
+++ ReactJs/reactjs-learning-app1/src/App.jsx
@@ -1,49 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Search from "./components/search";
 import Spinner from "./components/spinner";
-import { getPopularMovies } from "./services/api";
+import {getPopularMovies} from "./services/api";
+import MovieCard from "./components/MovieCard.jsx";
+import {useDebounce} from "react-use";
 
 const App = () => {
-  const [searchText, setSearchText] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [movieList, setMovieList] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [debouncedSearchText, setDebouncedSearchText] = useState('');
 
-  useEffect(() => {
-    getPopularMovies(setErrorMessage, setMovieList, setIsLoading);
-  }, []);
+    useDebounce(() => {
+        setDebouncedSearchText(searchText);
+    }, 500, [searchText]);
 
-  return (
-    <main>
-      <div className="pattern" />
-      <div className="wrapper">
-        <header>
-          <img src="./hero.png" alt="Hero logo" />
-          <h1>
-            Find all <span className="text-gradient">Movies</span> you'll Enjoy
-          </h1>
-        </header>
-        <Search searchText={searchText} setSearchText={setSearchText} />
+    useEffect(() => {
+        getPopularMovies(setErrorMessage, setMovieList, setIsLoading, debouncedSearchText);
+    }, [debouncedSearchText]);
 
-        <section className="movies-list">
-          <h2>Popular Movies</h2>
-          <br/>
-          {isLoading ? (
-            <Spinner/>
-          ) : errorMessage ? (
-            <p className="text-red-500 error-message">{errorMessage}</p>
-          ) : (
-            <ul>
-              {movieList.map((movie) => (
-                <p key={movie.id} className="text-white">{movie.title}</p>
-              ))}
-            </ul>
-          )}
-        </section>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="/hero.png" alt="Hero logo"/>
+                    <h1>
+                        Find all <span className="text-gradient">Movies</span> you'll Enjoy
+                    </h1>
+                    <Search searchText={searchText} setSearchText={setSearchText}/>
+                </header>
+
+                <section className="movies-list all-movies">
+                    <h2 className="mt-[40px] mb-[40px]">Popular Movies</h2>
+                    {isLoading ? (
+                        <Spinner/>
+                    ) : errorMessage ? (
+                        <p className="text-red-500 error-message">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie}/>
+                            ))}
+                        </ul>
+                    )}
+                </section>
+            </div>
+        </main>
+    );
 };
 
 export default App;
--- ReactJs/reactjs-learning-app1/src/services/api.js
+++ ReactJs/reactjs-learning-app1/src/services/api.js
@@ -1,39 +1,42 @@
 const TMDB_API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const TMDB_API_URL = import.meta.env.VITE_TMDB_API_URL;
 const GET_API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${TMDB_API_KEY}`,
-  },
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${TMDB_API_KEY}`,
+    },
 };
 
 export const getPopularMovies = async (
-  setErrorMessage,
-  setMovieList,
-  setIsLoading
+    setErrorMessage,
+    setMovieList,
+    setIsLoading,
+    query
 ) => {
-  setIsLoading(true);
-  setErrorMessage("");
-  try {
-    const endpoint = `${TMDB_API_URL}/discover/movie?sort_by=popularity.desc`;
-    const response = await fetch(endpoint, GET_API_OPTIONS);
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+        const endpoint = query
+            ? `${TMDB_API_URL}/search/movie?query=${encodeURIComponent(query)}`
+            : `${TMDB_API_URL}/discover/movie?sort_by=popularity.desc`;
+        const response = await fetch(endpoint, GET_API_OPTIONS);
 
-    if (!response.ok) {
-      throw new Error(`API Error: ${response.status} ${response.statusText}`);
+        if (!response.ok) {
+            throw new Error(`API Error: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        if (data.response === "false") {
+            setErrorMessage(data.Error || "No movies found. Please try again later.");
+            setMovieList([]);
+            return;
+        }
+        setMovieList(data.results || []);
+    } catch (error) {
+        setErrorMessage("Something went wrong. Please try again later.");
+    } finally {
+        setIsLoading(false);
     }
-
-    const data = await response.json();
-
-    if (data.response === "false") {
-      setErrorMessage(data.Error || "No movies found. Please try again later.");
-      setMovieList([]);
-      return;
-    }
-    setMovieList(data.results || []);
-  } catch (error) {
-    setErrorMessage("Something went wrong. Please try again later.");
-  } finally {
-    setIsLoading(false);
-  }
 };
